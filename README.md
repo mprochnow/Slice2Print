090965e54b8f044429e112a34253a043fad67614
Martin J. Prochnow <email@martin-prochnow.de>
2019-06-25 20:08:29 +0200

    Clean-up and documentation

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 
 This is my attempt to find out if I can implement a slicer for 3D printing. Don't take this too serious.
 
+Implements "An improved slicing algorithm with efficient contour
+construction using STL files" by Zhengyan Zhang and Sanjay Joshi.
+
 ## Requirements
 * python-opengl
 * numpy
--- slice2print/slicer.py
+++ slice2print/slicer.py
@@ -13,6 +13,9 @@
 # You should have received a copy of the GNU General Public License
 # along with Slice2Print.  If not, see <http://www.gnu.org/licenses/>.
 
+# Implementation of "An improved slicing algorithm with efficient contour
+# construction using STL files" by Zhengyan Zhang and Sanjay Joshi
+
 import collections
 import math
 
@@ -22,6 +25,8 @@ VERTEX_PRECISION = 1000.0
 
 
 class Vertex:
+    __slots__ = ["x", "y", "z", "flag"]
+
     def __init__(self, x, y, z, flag=None):
         self.x = x
         self.y = y
@@ -37,11 +42,10 @@ class Vertex:
         yield self.y
         yield self.z
 
-    def __str__(self):
-        return "Vertex(x: %s, y: %s, z: %s, flag: %s)" % (self.x, self.y, self.z, self.flag)
-
 
 class Edge:
+    __slots__ = ["p", "q"]
+
     def __init__(self, p, q):
         """
         :param p: Instance of Vertex
@@ -78,32 +82,29 @@ class Edge:
     def __eq__(self, other):
         return self.p == other.p and self.q == other.q
 
-    def __str__(self):
-        return "Edge(p: %s, q: %s)" % (self.p, self.q)
-
 
 class Intersection:
     """
     Contains the intersection of a z plane with a triangle
     and the two edges of the triangle which were intersected
     """
-    def __init__(self, v_inter, forward_edge, backward_edge, layer):
-        self.v_inter = v_inter
+    __slots__ = ["intersection", "forward_edge", "backward_edge", "layer"]
+
+    def __init__(self, intersection, forward_edge, backward_edge, layer):
+        self.intersection = intersection
         self.forward_edge = forward_edge
         self.backward_edge = backward_edge
         self.layer = layer
 
-    def __str__(self):
-        return "Intersection(v_inter: %s, layer: %s)" % (self.v_inter, self.layer)
-
 
 class Triangle:
-    def __init__(self, v1, v2, v3, n):
+    __slots__ = ["v1", "v2", "v3", "vz_min", "vz_med", "vz_max", "s1", "s2", "s3"]
+
+    def __init__(self, v1, v2, v3):
         """
         :param v1: numpy.array([x1, y1, z1])
         :param v2: numpy.array([x2, y2, z2])
         :param v3: numpy.array([x3, y3, z3])
-        :param n: numpy.array([x, y, z])
         """
         self.v1 = Vertex(*v1, 0)
         self.v2 = Vertex(*v2, 1)
@@ -169,15 +170,8 @@ class Triangle:
 
             yield Intersection(Vertex(x, y, z), upper_forward_edge, upper_backward_edge, layer)
 
-    def __str__(self):
-        return """Triangle(
-    lower forward edge: %s,
-    upper forward edge: %s,
-    lower backward edge: %s,
-    upper backward edge: %s)""" % self.get_forward_edge()
 
-
-class IntersectionList:
+class Intersections:
     """
     Contains the intersections of one layer
     """
@@ -192,12 +186,6 @@ class IntersectionList:
     @property
     def last(self):
         return self.intersections[-1]
-
-    def add_to_front(self, intersections):
-        self.intersections.extendleft(reversed(intersections.intersections))
-
-    def add_to_back(self, intersections):
-        self.intersections.extend(intersections.intersections)
 
     def is_adjacent_to_first_element(self, intersection):
         """
@@ -237,8 +225,20 @@ class IntersectionList:
 
         return result
 
+    def add_intersections_to_front(self, intersections):
+        """
+        :param intersections: Instance of Intersections
+        """
+        self.intersections.extendleft(reversed(intersections.intersections))
 
-class ContourList:
+    def add_intersections_to_back(self, intersections):
+        """
+        :param intersections: Instance of Intersections
+        """
+        self.intersections.extend(intersections.intersections)
+
+
+class Contour:
     """
     Contains the contour of one layer
     """
@@ -246,43 +246,45 @@ class ContourList:
         self.contour = collections.deque()
 
     def add(self, intersection):
-        for intersection_list in self.contour:
-            if intersection_list.add_to_front_if_adjacent(intersection):
-                self.add_to_back_if_adjacent(intersection_list)
+        """
+        :param intersection: Instance of Intersection
+        """
+        for intersections in self.contour:
+            if intersections.add_to_front_if_adjacent(intersection):
+                self.add_to_back_if_adjacent(intersections)
                 break
-            elif intersection_list.add_to_back_if_adjacent(intersection):
-                self.add_to_front_if_adjacent(intersection_list)
+            elif intersections.add_to_back_if_adjacent(intersection):
+                self.add_to_front_if_adjacent(intersections)
                 break
         else:
             # intersection was not added to an existing intersection list so create a new one
-            self.contour.append(IntersectionList(intersection))
+            self.contour.append(Intersections(intersection))
 
-    def add_to_back_if_adjacent(self, intersection_list):
-        intersection = intersection_list.first
-
-        for contour_part in self.contour:
-            if contour_part == intersection_list:
+    def add_to_back_if_adjacent(self, intersections_to_add):
+        """
+        :param intersections_to_add: Instance of Intersections
+        """
+        for intersections in self.contour:
+            if intersections == intersections_to_add:
                 continue
 
-            if contour_part.is_adjacent_to_last_element(intersection):
-                contour_part.add_to_back(intersection_list)
-                self.contour.remove(intersection_list)
+            if intersections.is_adjacent_to_last_element(intersections_to_add.first):
+                intersections.add_intersections_to_back(intersections_to_add)
+                self.contour.remove(intersections_to_add)
                 break
 
-    def add_to_front_if_adjacent(self, intersection_list):
-        intersection = intersection_list.last
-
-        for contour_part in self.contour:
-            if contour_part == intersection_list:
+    def add_to_front_if_adjacent(self, intersections_to_add):
+        """
+        :param intersections_to_add: Instance of Intersections
+        """
+        for intersections in self.contour:
+            if intersections == intersections_to_add:
                 continue
 
-            if contour_part.is_adjacent_to_first_element(intersection):
-                contour_part.add_to_front(intersection_list)
-                self.contour.remove(intersection_list)
+            if intersections.is_adjacent_to_first_element(intersections_to_add.last):
+                intersections.add_intersections_to_front(intersections_to_add)
+                self.contour.remove(intersections_to_add)
                 break
-
-    def __str__(self):
-        return "Contour(# of intersection lists: %s)" % len(self.contour)
 
 
 class Slicer:
@@ -308,7 +310,7 @@ class Slicer:
         """
         :param first_layer_height: in mm (e.g. 0.3)
         :param layer_height: in mm (e.g. 0.2)
-        :return: TODO
+        :return: list [[[x1, y1, z1], [x2, y2, z2]], ...] (for now)
         """
         slice_count = math.floor((self.model.dimensions.z - first_layer_height) / layer_height + 1)
 
@@ -317,13 +319,12 @@ class Slicer:
 
         slices = []
         for intersection in range(slice_count):
-            slices.append(ContourList())
+            slices.append(Contour())
 
         for intersection, j, k in self.indices:
             v1, v2, v3 = self.vertices[intersection], self.vertices[j], self.vertices[k]
-            n = self.normals[intersection]  # all normals of a face are the same
 
-            triangle = Triangle(v1, v2, v3, n)
+            triangle = Triangle(v1, v2, v3)
 
             if triangle.vz_min.z != triangle.vz_max.z:
                 for intersection in triangle.slice(first_layer_height, layer_height):
@@ -344,11 +345,11 @@ class Slicer:
                         p2 = intersection
 
                     if p1 is not None and p2 is not None:
-                        sliced_model.append([[*p1.v_inter], [*p2.v_inter]])
+                        sliced_model.append([[*p1.intersection], [*p2.intersection]])
 
                 if len(slice.contour[0].intersections) > 2:
-                    sliced_model.append([[*slice.contour[0].first.v_inter],
-                                         [*slice.contour[0].last.v_inter]])
+                    sliced_model.append([[*slice.contour[0].first.intersection],
+                                         [*slice.contour[0].last.intersection]])
 
         return sliced_model
 
