1fb483bdbd850b939de002b7b64fcafc6d4fdc64
Martin J. Prochnow <email@martin-prochnow.de>
2020-01-15 20:35:44 +0100

    Introduced functions to add spin controls to options panel

--- slice2print/slice2print.py
+++ slice2print/slice2print.py
@@ -130,96 +130,95 @@ class OptionsPanel(wx.Panel):
         wx.Panel.__init__(self, parent)
 
         sizer = wx.FlexGridSizer(0, 3, 0, 0)
-
-        # First layer height
-        sizer.Add(wx.StaticText(self, wx.ID_ANY, "First layer height"), 0, wx.ALIGN_CENTER_VERTICAL, 7)
-
-        self.ctrl_first_layer_height = wx.SpinCtrlDouble(self, wx.ID_ANY, min=0.0, style=wx.ALIGN_RIGHT | wx.SP_ARROW_KEYS)
-        self.ctrl_first_layer_height.SetDigits(2)
-        self.ctrl_first_layer_height.SetIncrement(0.1)
-        sizer.Add(self.ctrl_first_layer_height, 0, wx.EXPAND | wx.ALIGN_CENTER_VERTICAL | wx.LEFT, 7)
-
-        sizer.Add(wx.StaticText(self, wx.ID_ANY, "mm"), 0, wx.ALIGN_CENTER_VERTICAL | wx.LEFT, 7)
-
-        # Layer height
-        sizer.Add(wx.StaticText(self, wx.ID_ANY, "Layer height"), 0, wx.ALIGN_CENTER_VERTICAL | wx.TOP | wx.BOTTOM, 7)
-
-        self.ctrl_layer_height = wx.SpinCtrlDouble(self, wx.ID_ANY, min=0.0, style=wx.ALIGN_RIGHT | wx.SP_ARROW_KEYS)
-        self.ctrl_layer_height.SetDigits(2)
-        self.ctrl_layer_height.SetIncrement(0.1)
-        sizer.Add(self.ctrl_layer_height, 0, wx.EXPAND | wx.ALIGN_CENTER_VERTICAL | wx.LEFT | wx.TOP | wx.BOTTOM, 7)
-
-        sizer.Add(wx.StaticText(self, wx.ID_ANY, "mm"), 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 7)
-
-        # Perimeters
-        sizer.Add(wx.StaticText(self, wx.ID_ANY, "Perimeters"), 0, wx.ALIGN_CENTER_VERTICAL | wx.TOP | wx.BOTTOM, 7)
-
-        self.ctrl_perimeters = wx.SpinCtrl(self, wx.ID_ANY, min=1, style=wx.ALIGN_RIGHT | wx.SP_ARROW_KEYS)
-        sizer.Add(self.ctrl_perimeters, 0, wx.EXPAND | wx.ALIGN_CENTER_VERTICAL | wx.LEFT | wx.TOP | wx.BOTTOM, 7)
-
-        sizer.Add(wx.StaticText(self, wx.ID_ANY, ""), 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 7)
-
-        # Top and bottom layers, infill overlap
-        sizer.Add(wx.StaticText(self, wx.ID_ANY, "Top layers"), 0, wx.ALIGN_CENTER_VERTICAL | wx.TOP, 7)
-        self.ctrl_top_layers = wx.SpinCtrl(self, wx.ID_ANY, min=0, style=wx.ALIGN_RIGHT | wx.SP_ARROW_KEYS)
-        sizer.Add(self.ctrl_top_layers, 0, wx.EXPAND | wx.ALIGN_CENTER_VERTICAL | wx.LEFT | wx.TOP, 7)
-        sizer.Add(wx.StaticText(self, wx.ID_ANY, ""), 0, wx.ALIGN_CENTER_VERTICAL | wx.TOP, 7)
-
-        sizer.Add(wx.StaticText(self, wx.ID_ANY, "Bottom layers"), 0, wx.ALIGN_CENTER_VERTICAL | wx.TOP, 7)
-        self.ctrl_bottom_layers = wx.SpinCtrl(self, wx.ID_ANY, min=0, style=wx.ALIGN_RIGHT | wx.SP_ARROW_KEYS)
-        sizer.Add(self.ctrl_bottom_layers, 0, wx.EXPAND | wx.ALIGN_CENTER_VERTICAL | wx.LEFT | wx.TOP, 7)
-        sizer.Add(wx.StaticText(self, wx.ID_ANY, ""), 0, wx.ALIGN_CENTER_VERTICAL | wx.TOP, 7)
-
-        sizer.Add(wx.StaticText(self, wx.ID_ANY, "Infill overlap"), 0, wx.ALIGN_CENTER_VERTICAL | wx.TOP | wx.BOTTOM, 7)
-        self.ctrl_infill_overlap = wx.SpinCtrl(self, wx.ID_ANY, min=0, max=100, style=wx.ALIGN_RIGHT | wx.SP_ARROW_KEYS)
-        sizer.Add(self.ctrl_infill_overlap, 0, wx.EXPAND | wx.ALIGN_CENTER_VERTICAL | wx.LEFT | wx.TOP | wx.BOTTOM, 7)
-        sizer.Add(wx.StaticText(self, wx.ID_ANY, "%"), 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 7)
-
-        # First layer speed
-        sizer.Add(wx.StaticText(self, wx.ID_ANY, "First layer speed"), 0, wx.ALIGN_CENTER_VERTICAL | wx.TOP, 7)
-
-        self.ctrl_first_layer_speed = wx.SpinCtrl(self, wx.ID_ANY, min=1, style=wx.ALIGN_RIGHT | wx.SP_ARROW_KEYS)
-        sizer.Add(self.ctrl_first_layer_speed, 0, wx.EXPAND | wx.ALIGN_CENTER_VERTICAL | wx.LEFT | wx.TOP, 7)
-
-        sizer.Add(wx.StaticText(self, wx.ID_ANY, "mm/sec"), 0, wx.ALIGN_CENTER_VERTICAL | wx.LEFT | wx.TOP, 7)
-
-        # Print speed
-        sizer.Add(wx.StaticText(self, wx.ID_ANY, "Print speed"), 0, wx.ALIGN_CENTER_VERTICAL | wx.TOP, 7)
-
-        self.ctrl_print_speed = wx.SpinCtrl(self, wx.ID_ANY, min=1, style=wx.ALIGN_RIGHT | wx.SP_ARROW_KEYS)
-        sizer.Add(self.ctrl_print_speed, 0, wx.EXPAND | wx.ALIGN_CENTER_VERTICAL | wx.LEFT | wx.TOP, 7)
-
-        sizer.Add(wx.StaticText(self, wx.ID_ANY, "mm/sec"), 0, wx.ALIGN_CENTER_VERTICAL | wx.LEFT | wx.TOP, 7)
-
-        # Travel speed
-        sizer.Add(wx.StaticText(self, wx.ID_ANY, "Travel speed"), 0, wx.ALIGN_CENTER_VERTICAL | wx.TOP, 7)
-
-        self.ctrl_travel_speed = wx.SpinCtrl(self, wx.ID_ANY, min=1, style=wx.ALIGN_RIGHT | wx.SP_ARROW_KEYS)
-        sizer.Add(self.ctrl_travel_speed, 0, wx.EXPAND | wx.ALIGN_CENTER_VERTICAL | wx.LEFT | wx.TOP, 7)
-
-        sizer.Add(wx.StaticText(self, wx.ID_ANY, "mm/sec"), 0, wx.ALIGN_CENTER_VERTICAL | wx.LEFT | wx.TOP, 7)
-
         self.SetSizer(sizer)
-        self.Layout()
 
-        self.ctrl_first_layer_height.Bind(wx.EVT_SPINCTRLDOUBLE, self.on_update)
-        self.ctrl_layer_height.Bind(wx.EVT_SPINCTRLDOUBLE, self.on_update)
-        self.ctrl_perimeters.Bind(wx.EVT_SPINCTRL, self.on_update)
-        self.ctrl_top_layers.Bind(wx.EVT_SPINCTRL, self.on_update)
-        self.ctrl_bottom_layers.Bind(wx.EVT_SPINCTRL, self.on_update)
-        self.ctrl_bottom_layers.Bind(wx.EVT_SPINCTRL, self.on_update)
-        self.ctrl_infill_overlap.Bind(wx.EVT_SPINCTRL, self.on_update)
-        self.ctrl_print_speed.Bind(wx.EVT_SPINCTRL, self.on_update)
-        self.ctrl_travel_speed.Bind(wx.EVT_SPINCTRL, self.on_update)
+        self.ctrl_first_layer_height = self.add_spin_ctrl_double("First layer height", 0.0, 10.0, "mm")
+        self.ctrl_layer_height = self.add_spin_ctrl_double("Layer_height", 0.0, 10.0, "mm", True)
 
+        self.ctrl_perimeters = self.add_spin_ctrl("Perimeters", 1, 100, "", True)
+
+        self.ctrl_top_layers = self.add_spin_ctrl("Top layers", 0, 100)
+        self.ctrl_bottom_layers = self.add_spin_ctrl("Bottom layers", 0, 100)
+        self.ctrl_infill_overlap = self.add_spin_ctrl("Infill overlap", 0, 100, "%", True)
+
+        self.ctrl_first_layer_speed = self.add_spin_ctrl("First layer speed", 1, 1000, "mm/sec")
         self.ctrl_first_layer_speed.Disable()
+        self.ctrl_print_speed = self.add_spin_ctrl("Print speed", 1, 1000, "mm/sec")
         self.ctrl_print_speed.Disable()
+        self.ctrl_travel_speed = self.add_spin_ctrl("Travel speed", 1, 10000, "mm/src", True)
         self.ctrl_travel_speed.Disable()
+
+        self.Layout()
 
         self.controller.init_options(self)
 
     def on_update(self, event):
         self.controller.update_options(self)
+
+    def add_spin_ctrl_double(self, label, min_, max_, unit="", offset_bottom=False):
+        """
+        Adds a wx.SpinCtrlDouble to the Panel and sets its event handler for wx.EVT_SPINCTRLDOUBLE to self.on_update.
+        Control is configured with 2 digits and an increment of 0.1 for now.
+
+        :param label: Text for label in front of control
+        :param min_: Minimum value for the control
+        :param max_: Maximum value for the control
+        :param unit: Text for label behind the control
+        :param offset_bottom: Should the control be rendered with a bottom margin
+        :return: Instance of wx.SpinCtrlDouble
+        """
+        sizer = self.GetSizer()
+
+        margin = wx.TOP
+        if offset_bottom:
+            margin |= wx.BOTTOM
+
+        # Label in front of control
+        sizer.Add(wx.StaticText(self, wx.ID_ANY, label), 0, wx.ALIGN_CENTER_VERTICAL | margin, 7)
+
+        # The control itself
+        ctrl = wx.SpinCtrlDouble(self, wx.ID_ANY, min=0.0, style=wx.ALIGN_RIGHT | wx.SP_ARROW_KEYS)
+        ctrl.SetDigits(2)
+        ctrl.SetIncrement(0.1)
+        sizer.Add(ctrl, 0, wx.EXPAND | wx.ALIGN_CENTER_VERTICAL | wx.LEFT | margin, 7)
+
+        # Label behind the control
+        sizer.Add(wx.StaticText(self, wx.ID_ANY, "mm"), 0, wx.ALIGN_CENTER_VERTICAL | wx.LEFT | margin, 7)
+
+        ctrl.Bind(wx.EVT_SPINCTRLDOUBLE, self.on_update)
+
+        return ctrl
+
+    def add_spin_ctrl(self, label, min_, max_, unit="", offset_bottom=False):
+        """
+        Adds a wx.SpinCtrl to the Panel and sets its event handler for wx.EVT_SPINCTRL to self.on_update
+
+        :param label: Text for label in front of control
+        :param min_: Minimum value for the control
+        :param max_: Maximum value for the control
+        :param unit: Text for label behind the control
+        :param offset_bottom: Should the control be rendered with a bottom margin
+        :return: Instance of wx.SpinCtrl
+        """
+        sizer = self.GetSizer()
+
+        margin = wx.TOP
+        if offset_bottom:
+            margin |= wx.BOTTOM
+
+        # Label in front of control
+        sizer.Add(wx.StaticText(self, wx.ID_ANY, label), 0, wx.ALIGN_CENTER_VERTICAL | margin, 7)
+
+        # The control itself
+        ctrl = wx.SpinCtrl(self, wx.ID_ANY, min=min_, max=max_, style=wx.ALIGN_RIGHT | wx.SP_ARROW_KEYS)
+        sizer.Add(ctrl, 0, wx.EXPAND | wx.ALIGN_CENTER_VERTICAL | wx.LEFT | margin, 7)
+
+        # Label behind control
+        sizer.Add(wx.StaticText(self, wx.ID_ANY, unit), 0, wx.ALIGN_CENTER_VERTICAL | margin, 7)
+
+        ctrl.Bind(wx.EVT_SPINCTRL, self.on_update)
+
+        return ctrl
 
 
 class MainFrame(wx.Frame):
